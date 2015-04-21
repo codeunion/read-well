86c235a3a35a77ba74e041a731d183b1a00ecd71
Zee Spencer <zee@zeespencer.com>
2015-04-20 17:45:16 -0700

    Use devise for user registrations

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -7,8 +7,13 @@ RSpec.feature "Registration" do
     click_link_or_button "Sign up"
     fill_in "Email", with: "zee@example.com"
     fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
     click_link_or_button "Sign up"
 
-    expect(page).to have_content "Welcome zee@example.com"
+    # User is created
+    expect(User.find_by(email: "zee@example.com")).to be_present
+
+    # User is logged in
+    expect(page).to have_content "Welcome! You have signed up successfully"
   end
 end
